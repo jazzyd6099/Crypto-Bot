29dbfb4bb194be34905b8c14bc26dd7093b53d39
Jazzy <41112271+jazzyd6099@users.noreply.github.com>
2020-04-17 15:12:01 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,35 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+const prefix = "?"
+
+//embedColors
+
+const embedRed = 0xff0000
+const embedOrange = 0xff790c
+const embedYellow = 0xffff00
+const embedGreen = 0x00ff00
+const embedBlue = 0x0064ff
+const embedPurple = 0x6a00b0
+const embedMagenta = 0x9600ff
+const embedPink = 0xff00ff
+const embedBlack = 0x000000
+const embedWhite = 0xffffff
+const embedGray = 0x777777
 
 client.on("ready", () => {
-  console.log("I am ready");
+  console.log("I am ready.");
 });
 
 client.on("message", (message) => {
-  if (message.content.startsWith("ping")) {
-    message.channel.send("pong");
+  if (message.content.startsWith(prefix + "ping")) {
+    message.channel.send("pong.");
+  } else
+    if (message.content.startsWith(prefix + "server")) {
+	message.channel.send({embed: {
+              color: embedGreen,
+              title: "Server Information",
+              description: "Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}",
+   }});
   }
 });
 
