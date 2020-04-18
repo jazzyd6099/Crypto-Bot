72ea124badf63666edb5f10c73bbd6ba3e01f387
Jazzy <41112271+jazzyd6099@users.noreply.github.com>
2020-04-18 18:07:59 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -59,30 +59,12 @@ client.on('disconnect', () => {
 
 client.on('reconnecting', () => console.warn('Reconnecting...'))
 	
-	
-//embedColors
-
-const embedRed = 0xff0000
-const embedOrange = 0xff790c
-const embedYellow = 0xffff00
-const embedGreen = 0x00ff00
-const embedBlue = 0x0064ff
-const embedPurple = 0x6a00b0
-const embedMagenta = 0x9600ff
-const embedPink = 0xff00ff
-const embedBlack = 0x000000
-const embedWhite = 0xffffff
-const embedGray = 0x777777
 
 client.on("ready", () => {
   console.log("I am ready.");
 	  
 const activity = activities[Math.floor(Math.random() * activities.length)];
-    client.user.setPresence({
-      activity: {
-        name: activity.text,
-        type: activity.type
-      }
+client.user.setPresence({ activity: { name: activity.text }, status: 'idle' })
 	   .then(console.log)
   .catch(console.error);
 });
