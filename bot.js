35d802817a9d83eaecfbfecfbef7eebf34cd8137
Jazzy <41112271+jazzyd6099@users.noreply.github.com>
2020-04-18 17:59:14 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -84,9 +84,6 @@ const activity = activities[Math.floor(Math.random() * activities.length)];
         name: activity.text,
         type: activity.type
       }
-    });
-  .then(console.log)
-  .catch(console.error);
 });
 client.on("message", (message) => {
 	if (message.content.startsWith("Hey Crypto")) {
