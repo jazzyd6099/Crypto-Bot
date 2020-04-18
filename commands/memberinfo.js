5f82f0ab3070f1b3c291c6ead9981f85a8c4ab63
Jazzy <41112271+jazzyd6099@users.noreply.github.com>
2020-04-18 19:25:00 -0400

    Update memberinfo.js

--- commands/memberinfo.js
+++ commands/memberinfo.js
@@ -1,9 +1,9 @@
-module.exports.run = async (bot, message, args, funcs) => {
+module.exports.run = async (client, message, args, funcs) => {
 try {
         const embed = new Dicord.MessageEmbed()
             .setColor(funcs.rc())
             .setTitle('Server Member Info')
-            .addField('**__Info:__**', `Online users: ${message.guild.members.filter(m => m.presence.status == "online").size}\nOffline users: ${message.guild.members.filter(m => m.presence.status == "offline").size}\nTotal Users: ${message.guild.members.size}\nTotal Bots: ${message.guild.members.filter(m => m.user.bot == true).size}`)
+            .addField('**__Info:__**', `Online users: ${message.guild.members.filter(m => m.presence.status == "online").size}\nOffline users: ${message.guild.members.filter(m => m.presence.status == "offline").size}\nTotal Users: ${message.guild.members.size}\nTotal Bots: ${message.guild.members.filter(m => m.user.client == true).size}`)
         message.channel.send(embed);
     } catch (e) {
         console.error;
