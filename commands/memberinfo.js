448d64d8708344e8b68f0d1d2b7d6e73cab09435
Jazzy <41112271+jazzyd6099@users.noreply.github.com>
2020-04-18 19:34:52 -0400

    Update memberinfo.js

--- commands/memberinfo.js
+++ commands/memberinfo.js
@@ -9,4 +9,4 @@ const Discord = require('discord.js');
             .addField('**__Info:__**', `Online users: ${message.guild.members.filter(m => m.presence.status == "online").size}\nOffline users: ${message.guild.members.filter(m => m.presence.status == "offline").size}\nTotal Users: ${message.guild.members.size}\nTotal Bots: ${message.guild.members.filter(m => m.user.client == true).size}`)
         message.channel.send(embed);
     }
-};
+});
