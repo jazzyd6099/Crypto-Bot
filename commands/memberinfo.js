d95c724fd75e3e48d58ae4213f83d55b597a187c
Jazzy <41112271+jazzyd6099@users.noreply.github.com>
2020-04-18 19:33:34 -0400

    Update memberinfo.js

--- commands/memberinfo.js
+++ commands/memberinfo.js
@@ -1,17 +1,12 @@
-module.exports.run = async (client, message, args, funcs) => {
-try {
+const Discord = require('discord.js');
+        
+        client.on("message", (message) => {
+         if (!message.content.startsWith(prefix)) return;
+                if (message.content.startsWith(prefix+"userinfo")) {
         const embed = new Dicord.MessageEmbed()
             .setColor(funcs.rc())
             .setTitle('Server Member Info')
             .addField('**__Info:__**', `Online users: ${message.guild.members.filter(m => m.presence.status == "online").size}\nOffline users: ${message.guild.members.filter(m => m.presence.status == "offline").size}\nTotal Users: ${message.guild.members.size}\nTotal Bots: ${message.guild.members.filter(m => m.user.client == true).size}`)
         message.channel.send(embed);
-    } catch (e) {
-        console.error;
-        funcs.send(`Oh, An error occurred. \`${e.message}\`.`);
     }
-};
-
-module.exports.config = {
-    name: "memberinfo",
-    usage: "Use this command to get server member information.",
 };
