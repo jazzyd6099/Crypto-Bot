5342f6a28e8e932f691d70de8fa0327dc238e2e2
Jazzy <41112271+jazzyd6099@users.noreply.github.com>
2020-04-18 16:36:24 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -126,6 +126,25 @@ client.on("message", (message) => {
      			             .setImage(user.displayAvatarURL())
        				     .setColor(13101459)
 				    message.channel.send({embed})
+			    } else
+				    if (message.content.startsWith(prefix+"reactawait")) {
+					   message.react('👍').then(() => message.react('👎'));
+					    const filter = (reaction, user) => {
+			return ['👍', '👎'].includes(reaction.emoji.name) && user.id === message.author.id;
+		};
+					    message.awaitReactions(filter, { maxMatches: 1, time: 60000, errors: ['time'] })
+			.then(collected => {
+				const reaction = collected.first();
+
+				if (reaction.emoji.name === '👍') {
+					message.reply('you reacted with a thumbs up.');
+				} else {
+					message.reply('you reacted with a thumbs down.');	
+				}
+			})
+			.catch(collected => {
+				console.log(`After a minute, only ${collected.size} out of 4 reacted.`);
+				message.reply('you didn\'t react with neither a thumbs up, nor a thumbs down.');
 				  }
 		    });
 	client.on("message", (message) => {
