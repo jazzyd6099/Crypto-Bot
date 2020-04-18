c61593c7ad23cff21e65f5310ebb2bb3008c292c
Jazzy <41112271+jazzyd6099@users.noreply.github.com>
2020-04-17 23:27:33 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -57,6 +57,9 @@ client.on("message", (message) => {
 				  }
 		    });
 	client.on("message", (message) => {
+		if message.content.incluses("!can i hold your hand")) {
+			message.reply("No.");
+		} else
 		if (message.content.includes("need my commands? Okay, here they are.")) {
 			message.channel.send({embed: {
 				color: 13101459,
@@ -76,7 +79,7 @@ client.on("message", (message) => {
 					 },
 					 {
 						 name: "**User**",
-						 value: "?userinfo"
+						 value: "?userinfo, ?avatar"
 					 }
 					 ],
 				}});
