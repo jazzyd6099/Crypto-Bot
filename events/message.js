ff799550fb93243fceef165d7cd98f179c6e167d
Jazzy <41112271+jazzyd6099@users.noreply.github.com>
2020-04-22 00:36:17 -0400

    Create message.js

---
+++ events/message.js
@@ -0,0 +1,14 @@
+module.exports = (client, message) => {
+  // Ignore all bots
+  if (message.author.bot) return;
+  
+    const args = message.content.slice(client.config.prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+
+
+  const cmd = client.commands.get(command);
+
+  if (!cmd) return;
+
+  cmd.run(client, message, args);
+};
