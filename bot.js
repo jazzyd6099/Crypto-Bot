bb9dc1b9cc4101d79c0c78621254e29fa9b90964
Jazzy <41112271+jazzyd6099@users.noreply.github.com>
2020-04-22 01:20:03 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,27 +4,7 @@ const { prefix } = require('./config.json');
 const activities = require('./jsons/activity');
 
 const client = new Discord.Client();
-client.commands = new Discord.Collection();
 
-fs.readdir("./events/", (err, files) => {
-  if (err) return console.error(err);
-  files.forEach(file => {
-    const event = require(`./events/${file}`);
-    let eventName = file.split(".")[0];
-    client.on(eventName, event.bind(null, client));
-  });
-});
-
-fs.readdir("./commands/", (err, files) => {
-  if (err) return console.error(err);
-  files.forEach(file => {
-    if (!file.endsWith(".js")) return;
-    let props = require(`./commands/${file}`);
-    let commandName = file.split(".")[0];
-    console.log(`Attempting to load command ${commandName}`);
-    client.commands.set(commandName, props);
-  });
-});
 
 		    
 var x = [
