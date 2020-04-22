59b1c1187bb581ba32cc024ac6842a0bc7c6e0b7
Jazzy <41112271+jazzyd6099@users.noreply.github.com>
2020-04-22 00:40:46 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,16 +3,32 @@ const Discord = require("discord.js");
 const { prefix } = require('./config.json');
 const activities = require('./jsons/activity');
 
+client.config = config;
+
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
 
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+fs.readdir("./events/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    const event = require(`./events/${file}`);
+    let eventName = file.split(".")[0];
+    client.on(eventName, event.bind(null, client));
+  });
+});
 
-for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	client.commands.set(command.name, command);
-}
+client.commands = new Enmap();
 
+fs.readdir("./commands/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    if (!file.endsWith(".js")) return;
+    let props = require(`./commands/${file}`);
+    let commandName = file.split(".")[0];
+    console.log(`Attempting to load command ${commandName}`);
+    client.commands.set(commandName, props);
+  });
+});
 
 		    
 var x = [
