dec8f33f1ea1cb8862e71ada19ce99eca5436465
Jazzy <41112271+jazzyd6099@users.noreply.github.com>
2020-04-22 00:44:42 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require("./config.json");
 const prefix = "?"
+const { promisify } = require("util");
+const readdir = promisify(require("fs").readdir);
+const Enmap = require("enmap");
