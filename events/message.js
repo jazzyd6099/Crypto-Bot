466ff95bf10dd81c52efdbcec1bdca04f66ee06d
Jazzy <41112271+jazzyd6099@users.noreply.github.com>
2020-04-22 00:54:57 -0400

    Update message.js

--- events/message.js
+++ events/message.js
@@ -1,5 +1,6 @@
 module.exports = (client, message) => {
-  
+  const prefix = "?"
+
     const args = message.content.slice(client.config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
 
