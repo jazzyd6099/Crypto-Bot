b00fa143fc478da02eff82e0be96ec2ac2d87533
Jazzy <41112271+jazzyd6099@users.noreply.github.com>
2020-08-10 06:45:36 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -197,13 +197,14 @@ client.on('message', async(message) => {
 																let channel = message.mentions.channels.first();
 																if(message.mentions.channels.size < 1) return message.channel.send("You forgot to mention a channel for me to put the poll in.");
 																
-																let question = args.slice(0).join(' ');
+																let question = args.slice(1).join(' ');
+																if (question.length < 101 && question.length > 11) return true;
     															if (!question) return message.channel.send("You need to provide a question for the poll.");
 																
-																let option1 = args.slice(1).join(',')
+																let option1 = args.slice(2).join(',')
 																if (!option1) return message.channel.send("Provide an option.");
 																
-																let option2 = args.slice(2).join(' ')
+																let option2 = args.slice(3).join(' ')
 																if (!option2) return message.channel.send("provide another option.");
 																    
 																var Embed = new Discord.MessageEmbed()
