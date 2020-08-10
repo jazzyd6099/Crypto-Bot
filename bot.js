4a326b0f894a133346973b7741fedc7a5aac11e9
Jazzy <41112271+jazzyd6099@users.noreply.github.com>
2020-08-10 06:54:47 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -199,7 +199,6 @@ client.on('message', async(message) => {
 																
 																let question = args.slice(1).join(' ');
     															if (!question) return message.channel.send("You need to provide a question for the poll.");
-																if (question.length < 101 && question.length > 11) return true;
 																
 																let option1 = args.slice(2).join(',')
 																if (!option1) return message.channel.send("Provide an option.");
