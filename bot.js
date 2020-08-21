80e7f5e733a64af18a039f35bd0a9753879cd655
Jazzy <41112271+jazzyd6099@users.noreply.github.com>
2020-08-20 23:24:13 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -254,6 +254,9 @@ client.on('message', async(message) => {
 					  if (reaction.message.partial) await reaction.message.fetch();
 					  if (reaction.partial) await reaction.fetch();
 						  
+						  if(user.bot) return;
+						  if(!reaction.message.guild) return;
+						  
 						  if (reaction.message.channel.id === "746203022358478909") {
 						  if (reaction.emoji.name === '🟩'){
 							  await reaction.message.guild.members.cache.get(user.id).roles.add("746203940282040462")
