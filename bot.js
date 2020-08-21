a5b1ea9c7d58fa5bc1c64bb7214f8487acc34ce8
Jazzy <41112271+jazzyd6099@users.noreply.github.com>
2020-08-21 08:51:20 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -158,7 +158,7 @@ client.on('message', async message => {
 						.setColor(13101459)
 						.setDescription("These are my commands. There will be more to come. My prefix is self explanatory.")
 						.setAuthor('Crypto', 'https://i.postimg.cc/RCnXZHqy/cryptooooo.png')
-						.addField('__Server__','`?serverinfo`')
+						.addField('__Server__','`?serverinfo`, `?reactrole`')
 						.addField('__Cryptage/Interactions__', '`?roll, ?do you love me`, `?whatami`, `?pickalegend`, `?poll`, `?vote`')
 						.addField('__User__', '`?userinfo`, `?avatar`')
 						.setFooter('Bot coded and created by SpaceCarame#6433.')
