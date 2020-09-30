3223e79d13144427003d84fd42dbb9cdbdffe57a
Jazzy <41112271+jazzyd6099@users.noreply.github.com>
2020-09-29 22:23:36 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -159,10 +159,9 @@ client.on('guildMemberAdd', member => {
 
 	var embed = new Discord.MessageEmbed()
 	.setTitle(`Welcome to ${member.guild}.`)
-	.setDescription(`${member}`)
 	.setColor(13101459)
 	.setThumbnail(member.user.displayAvatarURL())
-	.setDescription("Heh, didn't expect you to show up here. May you have a great time.\n\nYou can go to #🔧roles🔧 to get roles to different channels.")
+	.setDescription(`Heh, didn't expect you to show up here, ${member.user.username}. May you have a great time.\n\nYou can go to #🔧roles🔧 to get roles to different channels.`)
 	channel.send({embed})
 });
 client.on('message', async message => {
